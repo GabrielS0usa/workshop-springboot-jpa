a78335623a4c9e72a0c91c754f5f5c4b7819a4a1
Gabriel_Sousa <gabriel.sist.inf@gmail.com>
2024-09-15 10:17:49 -0300

    Service layercomponent registration

--- src/main/java/com/gsweb/course/resources/UserResource.java
+++ src/main/java/com/gsweb/course/resources/UserResource.java
@@ -1,20 +1,34 @@
 package com.gsweb.course.resources;
 
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.gsweb.course.entities.User;
+import com.gsweb.course.services.UserService;
 
 @RestController
 @RequestMapping(value = "/users")
 public class UserResource {
 	
+	
+	private UserService service;
+	
 	@GetMapping
-	public ResponseEntity<User> findAll() {
-		User u = new User(1L, "Maria", "maria@gmail.com", "981912241", "1234");
-		return ResponseEntity.ok().body(u);
+	public ResponseEntity<List<User>> findAll() {
+		List<User> list = service.findAll();
+		return ResponseEntity.ok().body(list);
+	}
+	
+	@GetMapping(value = "/{id}")
+	public ResponseEntity<User> findById(@PathVariable Long id) {
+		User obj = service.findbyId(id);
+		return ResponseEntity.ok().body(obj);
 	}
 	
 }
