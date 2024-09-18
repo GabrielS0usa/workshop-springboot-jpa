d0c7592a39d6a743aff2014371887e8762fabdd0
Gabriel_Sousa <gabriel.sist.inf@gmail.com>
2024-09-18 12:29:50 -0300

    Creat entity category

--- src/main/java/com/gsweb/course/config/TestConfig.java
+++ src/main/java/com/gsweb/course/config/TestConfig.java
@@ -8,9 +8,11 @@ import org.springframework.boot.CommandLineRunner;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Profile;
 
+import com.gsweb.course.entities.Category;
 import com.gsweb.course.entities.Order;
 import com.gsweb.course.entities.User;
 import com.gsweb.course.entities.enums.OrderStatus;
+import com.gsweb.course.repositories.CategoryRepository;
 import com.gsweb.course.repositories.OrderRepository;
 import com.gsweb.course.repositories.UserRepository;
 
@@ -23,6 +25,9 @@ public class TestConfig implements CommandLineRunner {
 	
 	@Autowired
 	private OrderRepository orderRepository;
+	
+	@Autowired
+	private CategoryRepository categoryRepository;
 
 	@Override
 	public void run(String... args) throws Exception {
@@ -38,5 +43,10 @@ public class TestConfig implements CommandLineRunner {
 		
 		orderRepository.saveAll(Arrays.asList(o1, o2, o3));
 		
+		Category cat1 = new Category(null, "Electronics");
+		Category cat2 = new Category(null, "Books");
+		Category cat3 = new Category(null, "Computers");
+		
+		categoryRepository.saveAll(Arrays.asList(cat1, cat2, cat3));
 	}
 }
